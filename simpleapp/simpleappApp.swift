206f96cee79015606cfa93e7e5950ddf56891423
Cundy Sunardy <cundyzheng@gmail.com>
2024-11-27 16:58:34 +0700

    Initial Commit

---
+++ simpleapp/simpleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleappApp.swift
+//  simpleapp
+//
+//  Created by Cundy Sunardy on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct simpleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
